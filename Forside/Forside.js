6056ba93dfdb4d7855a71ab91cf8183ca86c352a
Phil2567 <phdu19ab@student.cbs.dk>
2019-10-15 13:11:08 +0200

    :F javascript til login + oprettelse

--- Forside/Forside.js
+++ Forside/Forside.js
@@ -1,15 +1,20 @@
 // Email og kodeord for oprettelse af bruger
 var username = document.getElementById('e-mail');
 var password = document.getElementById('kodeord');
+var telefon = document.getElementById('tlf');
+var name = document.getElementById('navn');
 var password = document.getElementById('bekræft_kodeord');
+
 
 // Store input from registration form in localStorage
 function storeLogin() {
     localStorage.setItem('e-mail', username.value);
     localStorage.setItem('kodeord', password.value);
     localStorage.setItem('bekræft_kodeord', password.value);
-    alert('Ny bruger oprettet');
-    onclick="window.location.href = 'Forside.html';
+    localStorage.setItem('navn', name.value);
+    localStorage.setItem('tlf', telefon.value);
+
+    window.open("Forside.html")
 }
 
 
@@ -25,14 +30,13 @@ function checkLogin() {
     // Stored data from the registration form
     var storedEmail = localStorage.getItem('e-mail');
     var storedPass = localStorage.getItem('kodeord');
-    var storedPass = localStorage.getItem('bekræft_kodeord');
 
     // Entered data in the login form
-    var enteredName = document.getElementById('enteredEmail');
+    var enteredEmail = document.getElementById('enteredEmail');
     var enteredPass = document.getElementById('enteredPass');
-    var enteredPass = document.getElementById('enteredPass');
+
     // Check if stored data from registration form is equal to entered data from login form
-    if(enteredName.value == storedEmail && enteredPass.value == storedPass) {
+    if(enteredEmail.value == storedEmail && enteredPass.value == storedPass) {
         alert('You are logged in.');
     }else {
         alert('Brugernavn eller kordeord er ikke korrekt.');
