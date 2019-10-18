d06c61fadf007561a8aa25036a27d275149f02ee
sashasommer <saso19ad@stuende>
2019-10-18 11:29:36 +0200

    hey

--- Brugeren/Brugeren.js
+++ Brugeren/Brugeren.js
@@ -1,2 +1,7 @@
 //Navn, email, kodeord, telefonnummer, opret, slet, (evt. facebook)
-
+function myFunction() {
+    alert("Du vil nu blive henvist til siden hvor du kan foretage en ny booking!")
+}
+function myFunction1() {
+    alert("Du vil nu blive henvist til siden hvor du kan annullere bookinger!")
+}
