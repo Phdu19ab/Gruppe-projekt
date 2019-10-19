a38299bf993e22da02264cf0246b774241ee16ea
Phil2567 <phdu19ab@student.cbs.dk>
2019-10-19 17:49:25 +0200

    :F Javascript til funktionen så den directer til nye sider

--- Brugeren/Brugeren.js
+++ Brugeren/Brugeren.js
@@ -1,7 +1,9 @@
 //Navn, email, kodeord, telefonnummer, opret, slet, (evt. facebook)
 function myFunction() {
     alert("Du vil nu blive henvist til siden hvor du kan foretage en ny booking!")
+    window.open("../Booking/Booking.html");
 }
 function myFunction1() {
     alert("Du vil nu blive henvist til siden hvor du kan annullere bookinger!")
+    window.open("../Afbestilling/Afbestilling.html")
 }
