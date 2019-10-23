c7eaafb311f2ff2c2ad7c450cff16c114f475710
sashasommer <saso19ad@stuende>
2019-10-23 15:05:04 +0200

    hey

--- Afbestilling/Afbestilling.js
+++ Afbestilling/Afbestilling.js
@@ -1,8 +1,9 @@
 //Booking oversigt, valg af booking - bekræft eller annuller
 function myFunction() {
-    alert("Din booking er nu annulleret, vi håber at du kommer tilbage en anden gang!")
+    alert("Din booking er nu annulleret, vi håber at du kommer tilbage en anden gang!");
 }
 
 function myFunction1() {
-    alert("Du har beholdt din booking, vi glæder os til at se dig!")
+    alert("Du har beholdt din booking, vi glæder os til at se dig!");
+    window.open("../Brugeren/Brugeren.html");
 }
--- Booking_bekræftelse/Booking_bekræftelse.js
+++ Booking_bekræftelse/Booking_bekræftelse.js
@@ -1,9 +1,3 @@
-function myFunction() {
-    alert("Du vil modtage bekræftelse via SMS!")
-}
-function myFunction1() {
-    alert("Du vil modtage bekræftelse via Email!")
-}
 
 function myFunction2() {
     alert("Du vil her kunne annullere din booking!")
