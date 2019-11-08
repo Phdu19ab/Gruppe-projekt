c7f893ed185fe0d21c2a7aa438ce68f491d7c3b6
Phil2567 <phdu19ab@student.cbs.dk>
2019-11-08 08:25:29 +0100

    :F Javascript til booking alert

--- Booking/Booking.js
+++ Booking/Booking.js
@@ -29,17 +29,26 @@ function showUser() {
 }
 
 function myFunction1() {
-    alert("Din booking er nu bekræftet og vi glæder os til at se dig/jer!" +
+    var isConfirmed=confirm("\n Din booking er nu bekræftet og vi glæder os til at se dig/jer!" +
         "\n Antal Personer: " + personer.options[personer.selectedIndex].value
         + "\n Booking dato: " + kontaktdag.options[kontaktdag.selectedIndex].value +
         "/" + kontaktmaaned.options[kontaktmaaned.selectedIndex].value +
         "/" + kontaktaar.options[kontaktaar.selectedIndex].value +
         "\n Tidspunkt for reservation af bord: " + tidspunkt.options[tidspunkt.selectedIndex].value +
         "\n Booking tidspunkt: " + currentDate +
-        "\n Ydeligere kommentare til Café Bomhuset: " + info1);
-    window.open("../Booking_bekræftelse/Booking_bekræftelse.html");
-      storeBooking();
+        "\n Ydeligere kommentare til Café Bomhuset: " + info1.value);
+    if (isConfirmed) {alert('Din Booking er nu bekræftet')
+        window.open("../Booking_bekræftelse/Booking_bekræftelse.html");
+    storeBooking();
+    } else {
+        alert("Din Booking er nu annuleret")
+        window.open("Booking.html");
+    }
 }
+
+
+
+
 
 
 
