d701df1138dd6421ceb2733f1bfea99369afb026
Phil2567 <phdu19ab@student.cbs.dk>
2019-11-08 08:37:47 +0100

    :F Javascript til booking alert

--- Booking/Booking.js
+++ Booking/Booking.js
@@ -1,8 +1,4 @@
-//Herunder er funktionen for knappen "Din booking er nu annulleret.." samt at der ved tryk på knappen åbnes et nyt linket vindue*/
-function myFunction() {
-    alert("Din booking er nu annulleret, vi håber at du kommer tilbage en anden gang!");
-        window.open("../Afbestilling/Afbestilling.html");
-}
+
 
  var personer = document.getElementById('Antal_Personer');
  var tidspunkt = document.getElementById('Tidspunkt');
@@ -45,6 +41,8 @@ function myFunction1() {
         window.open("Booking.html");
     }
 }
+
+
 
 
 
