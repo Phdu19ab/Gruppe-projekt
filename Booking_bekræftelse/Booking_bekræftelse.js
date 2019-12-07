3dbf909d7ab1e412b77094733bf84cccc9c4c098
gustav <guje19ac@student.cbs.dk>
2019-12-07 14:39:28 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Brugeren/Brugeren.html
    #	Brugeren/Brugeren.js

--- Booking_bekræftelse/Booking_bekræftelse.js
+++ Booking_bekræftelse/Booking_bekræftelse.js
@@ -1,8 +1,3 @@
-function myFunction2() {
-    alert("Du vil her kunne annullere din booking!");
-    window.location.href = "../Afbestilling/Afbestilling.html";
-}
-
 function myFunction3() {
     alert("Du vil nu modtage bekræftelse på din valgte enhed. Vi glæder os til at se dig!");
     window.location.href = "../Brugeren/index.html";
