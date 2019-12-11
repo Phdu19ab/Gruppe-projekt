e8d4cc57b7a8b385c39311a364cf3e8f504b17f8
gustav <guje19ac@student.cbs.dk>
2019-12-11 18:47:42 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Brugeren/Brugeren.html
    #	Brugeren/Brugeren.js

--- Booking_bekræftelse/Booking_bekræftelse.js
+++ Booking_bekræftelse/Booking_bekræftelse.js
@@ -4,29 +4,4 @@ function myFunction3() {
 
 }
 
-var nodemailer = require('nodemailer');
-
-var transporter = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: 'minmail@gmail.com',
-        pass: 'mitkodeord'
-    }
-});
-
-var mailOptions = {
-    from: 'minmail@gmail.com',
-    to: 'restaurantbruger@yahoo.com',
-    subject: 'Sending Email using Node.js',
-    html: '<h1>Din bookinger nu bekræftet!</h1> <p>Vi glæder os til at se dig på den pågældende dato</p>'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-        console.log(error);
-    } else {
-        console.log('Email sent: ' + info.response);
-    }
-});
-
 
